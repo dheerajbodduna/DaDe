3d3f4e6d228e84e88f3814e38ad51c66a3b8a94d
DheerajBodduna <166221254+dheerajbodduna@users.noreply.github.com>
2025-11-29 12:58:25 +0530

    Update Mock.py
    Da Changes

--- Mock.py
+++ Mock.py
@@ -121,4 +121,101 @@ df redshift_load(data):
 
 # Main Code Here : [def main()] 
 
-    
+
+## Machine Learning Hands on:
+
+# Import Dependencies 
+
+import pandas as pd
+from sklearn.utils import resample 
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, classification_report
+
+import warnings
+import boto3
+from sagemaker import get_execution_role
+
+warnings.filterwarnings('ignore')
+
+# Task 1 - Data Loading
+# Hint: sample s3 URL - "s3://bucket_name/folder_name/file_name.csv" 
+
+import numpy as np
+import pandas as pd
+from sklearn import datasets
+import sagemaker
+from sagemaker import get_execution_role
+role = get_execution_role()
+
+bucket_name = "loan-data202511291100" 
+folder_name = "loan_cleaned_data" 
+data_key = "loan_cleaned.csv" 
+
+data_location = f"s3://{bucket_name}/{folder_name}/{data_key}"
+
+data = pd.read_csv(data_location)
+data.head()
+
+# Task 2 - Feature Engineering 
+
+data = pd.get_dummies(data, columns=["purpose"], dtype=int)
+data.head()
+
+# Task 3 - Data preprocessing 
+
+from sklearn.utils import resample 
+
+print(data["not_fully_paid"].value_counts())
+
+df_majority = data[data["not_fully_paid"]==0]
+df_minority = data[data["not_fully_paid"]==1]
+
+# Handle the imbalanced data using resample method and oversample the minority class.
+df_minority_upsampled = resample(df_minority, replace=True, n_samples= df_majority.shape[0], random_state=42)
+
+# Concatenate the upsampled data records with the majority class records and shuffle the resultant dataframe
+df_balanced = pd.concat([df_majority, df_minority_unsampled])
+print(df_balanced['not_fully_paid].value_counts())
+
+# Task 4 - Model training 
+
+# Create X and y for Train-Test split
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier 
+
+X = df_balanced.drop("not_fully_paid", axis=1)
+y = df_balaced["not_fully_paid"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+
+# Train a RandomForestClassifier Model
+rf = RandomForestClassifier(random_state=42)
+rf.fit(X_train, y_train)
+
+# Task 5 - Model evaluation 
+
+# Predict using the training RandomForestClassifier Model
+from sklearn.metrics import classification_report
+y_pred = rf.predict(X_test)
+
+# Print the Classification report 
+print(classification_report(y_test, y_pred))
+
+# Task 6 - Saving the model to s3
+
+# Uploading the model to s3 using boto3
+import boto3
+import joblib
+import tempfile
+
+with tempfile.NamedTemporaryFile as tmp:
+    joblib.dump(rf, tmp.name)
+    tmp.flush()
+
+    s3 = boto3.client('s3')
+    s3.upload_file(tmp.name, bucket_name, "model.pkl")
+    print("Model saved to s3 bucket", f"s3://{bucket_name}/model.pkl")
+
